224b8db11a6fad3e6250528f9afae3fcec008504
Ben Dunn <dunn.bobcat@gmail.com>
2025-03-25 20:52:31 -0700

    Add WebIDL and FilterEffects

---
+++ definitions/WebIDL.js
@@ -0,0 +1,57 @@
+type AllowSharedBufferSource =
+  | ArrayBuffer
+  | SharedArrayBuffer
+  | ArrayBufferView;
+
+type ArrayBufferView =
+  | Int8Array
+  | Int16Array
+  | Int32Array
+  | Uint8Array
+  | Uint16Array
+  | Uint32Array
+  | Uint8ClampedArray
+  | BigInt64Array
+  | BigUint64Array
+  | Float16Array
+  | Float32Array
+  | Float64Array
+  | DataView;
+
+type BufferSource = ArrayBufferView | ArrayBuffer;
+
+declare class DOMException {
+  static +ABORT_ERR: 20;
+  static +DATA_CLONE_ERR: 25;
+  static +DOMSTRING_SIZE_ERR: 2;
+  static +HIERARCHY_REQUEST_ERR: 3;
+  static +INDEX_SIZE_ERR: 1;
+  static +INUSE_ATTRIBUTE_ERR: 10;
+  static +INVALID_ACCESS_ERR: 15;
+  static +INVALID_CHARACTER_ERR: 5;
+  static +INVALID_MODIFICATION_ERR: 13;
+  static +INVALID_NODE_TYPE_ERR: 24;
+  static +INVALID_STATE_ERR: 11;
+  static +NAMESPACE_ERR: 14;
+  static +NETWORK_ERR: 19;
+  static +NO_DATA_ALLOWED_ERR: 6;
+  static +NO_MODIFICATION_ALLOWED_ERR: 7;
+  static +NOT_FOUND_ERR: 8;
+  static +NOT_SUPPORTED_ERR: 9;
+  static +QUOTA_EXCEEDED_ERR: 22;
+  static +SECURITY_ERR: 18;
+  static +SYNTAX_ERR: 12;
+  static +TIMEOUT_ERR: 23;
+  static +TYPE_MISMATCH_ERR: 17;
+  static +URL_MISMATCH_ERR: 21;
+  static +VALIDATION_ERR: 16;
+  static +WRONG_DOCUMENT_ERR: 4;
+
+  +code: number;
+  +message: string;
+  +name: string;
+
+  constructor(message?: string, name?: string): void;
+}
+
+type VoidFunction = () => void;
