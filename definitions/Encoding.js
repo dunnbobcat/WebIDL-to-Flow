40be98125fe73b606e4c74b9e868eae4636fea0a
Ben Dunn <dunn.bobcat@gmail.com>
2025-03-25 22:04:20 -0700

    add Encoding

---
+++ definitions/Encoding.js
@@ -0,0 +1,51 @@
+type TextDecodeOptions = {
+  stream: boolean,
+};
+
+type TextDecoderOptions = {
+  fatal: boolean,
+  ignoreBOM: boolean,
+};
+
+type TextEncoderEncodeIntoResult = {
+  read: number,
+  written: number,
+};
+
+interface TextDecoderCommon {
+  +encoding: string;
+  +fatal: boolean;
+  +ignoreBOM: boolean;
+}
+
+interface TextEncoderCommon {
+  +encoding: string;
+}
+
+declare class TextDecoder mixins TextDecoderCommon {
+  constructor(label?: string, options?: TextDecoderOptions): void;
+
+  decode(input?: AllowSharedBufferSource, options?: TextDecodeOptions): string;
+}
+
+declare class TextDecoderStream
+  mixins TextDecoderCommon, GenericTransformStream
+{
+  constructor(label?: string, options?: TextDecoderOptions): void;
+}
+
+declare class TextEncoder mixins TextEncoderCommon {
+  constructor(): void;
+
+  encode(input?: string): Uint8Array;
+  encodeInto(
+    source: string,
+    destination: Uint8Array,
+  ): TextEncoderEncodeIntoResult;
+}
+
+declare class TextEncoderStream
+  mixins TextEncoderCommon, GenericTransformStream
+{
+  constructor(): void;
+}
